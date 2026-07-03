8b9bad8434b3396c52b05f1d335bded47de08432
LorisDrid <lorisdrid3000@gmail.com>
2026-07-03 10:03:32 +0200

    setup

---
+++ src/main/java/com/monacoevents/eventbooking/EventBookingApiApplication.java
@@ -0,0 +1,12 @@
+package com.monacoevents.eventbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBookingApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/monacoevents/eventbooking/EventBookingApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.monacoevents.eventbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
